19a04d98f4faf9fb29b10b22d66ff3a84848737e
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-22 17:54:25 -0500

    extracting dynamic parameter values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,15 @@
+import {useRouter} from 'next/router';
+// useRouter is customized React hook created by Next team
+
 // [newsId].js is a dynamic page
 
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // send request to backend API to fetch news item with newsId
+
   return (
     <h1>Detail Page</h1>
   )
