e4819c1581428b3ea6abfc2d99265e91d9f6852c
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-22 18:01:16 -0500

    using Link from next/link. does not refresh page, keeps SPA

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,23 +1,19 @@
+import Link from 'next/link';
+
 // our-domain.com/news
 
 function NewsPage() {
   return (
     <>
-    <h1>News</h1>
-    <ul>
-      <li>
-        <a href='/news/something-else'>
-              Celeste is financially abundant
-        </a>
-  
-      </li>
-      <li>
-        <a href='/'>
-           Celeste embodies peace
-        </a>
-       
+      <h1>News</h1>
+      <ul>
+        <li>
+          <Link href="/news/something-else">Celeste is financially abundant</Link>
         </li>
-    </ul>
+        <li>
+          <Link href="/">Celeste embodies peace</Link>
+        </li>
+      </ul>
     </>
   );
 }
