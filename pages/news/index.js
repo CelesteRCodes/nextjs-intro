e1e07760bbb7b92dd851890db91653a09161f29a
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-22 17:58:17 -0500

    linking between pages with a href - sends new request/gets new html pg, not SPA

--- pages/news/index.js
+++ pages/news/index.js
@@ -2,9 +2,24 @@
 
 function NewsPage() {
   return (
+    <>
     <h1>News</h1>
-  )
-
+    <ul>
+      <li>
+        <a href='/news/something-else'>
+              Celeste is financially abundant
+        </a>
+  
+      </li>
+      <li>
+        <a href='/'>
+           Celeste embodies peace
+        </a>
+       
+        </li>
+    </ul>
+    </>
+  );
 }
 
 export default NewsPage;
