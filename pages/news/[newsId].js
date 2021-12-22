68808f8d3e492b92447ae2998dafc92f14c433a5
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-22 17:49:59 -0500

    dynamic pages in nextjs

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,10 +1,9 @@
-// our-domain.com/news/something-important
+// [newsId].js is a dynamic page
 
 function DetailPage() {
   return (
     <h1>Detail Page</h1>
   )
-
 }
 
 export default DetailPage;
